2f0966e8fa17ff82516b14f4384fdda0480a5b87
harshamukund4070 <166089216+harshamukund4070@users.noreply.github.com>
2026-02-17 08:21:37 +0530

    Update program-25.py

---
+++ Lab_Programs/program-25.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("image-1.png",0)
+
+sobelx = cv2.Sobel(img,cv2.CV_64F,1,0)
+sobely = cv2.Sobel(img,cv2.CV_64F,0,1)
+gradient = cv2.magnitude(sobelx,sobely)
+
+cv2.imshow("Gradient", gradient)
+cv2.waitKey(0)
+cv2.destroyAllWindows
