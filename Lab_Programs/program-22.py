7cb51bc1d244a398ca02aa9350859146d2e98ce5
harshamukund4070 <166089216+harshamukund4070@users.noreply.github.com>
2026-02-17 08:20:42 +0530

    Update program-22.py

---
+++ Lab_Programs/program-22.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("image-2.png", 0)
+
+kernel = np.array([[0,-1,0],
+                   [-1,5,-1],
+                   [0,-1,0]])
+
+sharp = cv2.filter2D(img, -1, kernel)
+
+cv2.imshow("Sharpened", sharp)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
